6dc3d95655b57c18e793a5c1a7f870240a600dd0
Jacquelyn Kominik <jkominik@gmail.com>
2017-11-12 17:42:35 -0500

    adding the map

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107173819) do
+ActiveRecord::Schema.define(version: 20171112205543) do
 
-  create_table "templates", force: :cascade do |t|
-    t.string "title"
-    t.text "desc"
+  create_table "cans", force: :cascade do |t|
+    t.string "typeOfCan"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
