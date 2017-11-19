6bb3d16b4cd6b6651dc5d2467eacfdfbbf89d9d3
Santiago Vallejo <santiago.vallejo@smk.com.co>
2017-11-19 02:58:37 -0500

    fixing styles and misc

--- app/assets/javascripts/tour.js
+++ app/assets/javascripts/tour.js
@@ -1,34 +1,36 @@
-// $(function(){
-//   var introguide = introJs();
-//   // var startbtn   = $('#startdemotour');
-//   introguide.setOptions({
-//     steps: [
-//     {
-//       element: '#add-can .add-can',
-//       intro: 'Press here to add a can and later you can located and share with the community',
-//       position: 'bottom'
-//     },
-//     {
-//       element: '.find-can',
-//       intro: 'Click this find a the can near you!',
-//       position: 'bottom'
-//     },
-//     {
-//       element: '.trash-info',
-//       intro: 'Check here the different kind of trash',
-//       position: 'bottom'
-//     },
-//     {
-//       element: '.profile',
-//       intro: 'Click here to view and edit your profile',
-//       position: 'bottom'
-//     }
-//     // {
-//     //   element: '.nav-menu',
-//     //   intro: "Each demo will link to the previous & next entries.",
-//     //   position: 'bottom'
-//     // }
-//     ]
-//   });
-//   introguide.start();
-// });
+$(function(){
+  var introguide = introJs("#login").setOption("overlayOpacity", " 0 ");;
+  // var startbtn   = $('#startdemotour');
+  introguide.setOptions({
+    steps: [
+    {
+      element: '.introjs',
+      intro: 'Create an account to Find the near Can around you or to add a Can while you are walking.',
+      position: 'top'
+    }
+    // {
+    //   element: '.find-can',
+    //   intro: 'Click this find a the can near you!',
+    //   position: 'bottom'
+    // },
+    // {
+    //   element: '.trash-info',
+    //   intro: 'Check here the different kind of trash',
+    //   position: 'bottom'
+    // },
+    // {
+    //   element: '.profile',
+    //   intro: 'Click here to view and edit your profile',
+    //   position: 'bottom'
+    // }
+    // {
+    //   element: '.nav-menu',
+    //   intro: "Each demo will link to the previous & next entries.",
+    //   position: 'bottom'
+    // }
+    ]
+  });
+  introguide.start();
+});
+
+
