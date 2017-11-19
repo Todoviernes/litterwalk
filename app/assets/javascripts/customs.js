7eca9c1c929eb5d688c7a766a9ffb20ba027b569
Santiago Vallejo <santiago.vallejo@smk.com.co>
2017-11-19 00:26:43 -0500

    add active class to menu via jquery, add jquery to add facebook logo on button

--- app/assets/javascripts/customs.js
+++ app/assets/javascripts/customs.js
@@ -1,3 +1,22 @@
 function goBack() {
     window.history.back();
 }
+
+$( document ).on('turbolinks:load', function() {
+  console.log("It works on each visit!")
+  $( ".fb-btn" ).prepend( "<span><i class='fa fa-facebook-official'></i></span>" );
+  $(function(){
+    var current = location.pathname;
+    $('#footer a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        } else {
+        if($this.attr('href').indexOf(current) !== 1){
+        	$this.addClass('no-active');
+        }
+
+    }})
+})
+})
